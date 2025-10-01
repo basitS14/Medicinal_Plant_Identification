5adea8662d5f8256692aa4d464c31ffc6eb7c30d
basitS14 <abasitsolkar@gmail.com>
2025-10-02 01:01:56 +0530

    populating database

--- test.py
+++ test.py
@@ -1,3 +1,41 @@
-import tensorflow as tf
-print(tf.__version__)
-print(tf.reduce_sum(tf.random.normal([1000, 1000])))
+# from src.utils.helper import predict , preprocess  , load_model
+# from src.utils.helper import CNNetwork
+
+# model = load_model('./artifacts/models/torch_model.pth')
+
+# img_pth = './Medicinal Plant Identification Dataset/Validation Images/Curry leaf/Curry Leaf (6).jpg'
+# input_batch = preprocess(img_pth)
+# print(predict(model=model , input_batch=input_batch))
+
+from src.utils.database import PlantInfoDB
+import json
+
+db = PlantInfoDB()
+
+
+plants = [
+  {
+    "plantName": "Rubble Leaf",
+    "scientificName": "Ficus religiosa var. rubra", 
+    "family": "Moraceae",
+    "origin": "South Asia",
+    "benefits": [
+        "Traditionally used in folk remedies",
+        "Potential antioxidant properties"
+    ],
+    "recipes": [
+        {
+            "title": "Rubble Leaf Herbal Infusion",
+            "description": "Steep dried rubble leaves in hot water for 5-7 minutes. Consume in moderation."
+        }
+    ],
+    "toxicity": "Limited research available. Safe in small amounts, avoid excess until further studies.",
+    "isToxic": False
+}
+
+]
+
+
+for plant in plants:
+    res =    db.add_info(plant)
+    print(res)
